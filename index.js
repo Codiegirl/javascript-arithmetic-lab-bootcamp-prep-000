0bbadef7c52239a7283dadbe039cc5f7c891f8dc
Ro Flo <iroflood@msn.com>
2019-01-22 17:59:46 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,38 @@
+function add(a,b) {
+  return a + b;
+}
+
+function subtract(a,b) {
+  return a - b;
+}
+
+function multiply(a,b) {
+  return a*b;
+}
+ 
+function divide(a,b) {
+  return a/b;
+}
+
+function inc(a) {
+  return a += 1;
+}
+
+function dec(a) {
+  return a -= 1;
+}
+
+function makeInt(a, base) {
+  var parsed = parseInt(a, 10);
+  
+  return parsed;
+}
+
+
+function preserveDecimal(a) {
+  return parseFloat('a') * 2.222 * String.PI;
+}
+  
+  
+
+ 
